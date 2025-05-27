35ec764661c581279be6dbbf88131eca67a8c9fc
Brendan Jackman <bhenryj0117@gmail.com>
2025-05-27 20:14:43 +0200

    cli: Add subcommand parsing

--- src/falba/cli.py
+++ src/falba/cli.py
@@ -1,11 +1,22 @@
 import argparse
 
 
+def cmd_ab(args):
+    print(f"hello world {args.expr}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Falba CLI")
-    parser.parse_args()
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.required = True  # Ensures a subcommand must be specified
 
-    print("hello world")
+    ab_parser = subparsers.add_parser("ab", help="Run A/B test")
+    ab_parser.add_argument("expr")
+    ab_parser.set_defaults(func=cmd_ab)
+
+    args = parser.parse_args()
+
+    args.func(args)
 
 
 if __name__ == "__main__":
